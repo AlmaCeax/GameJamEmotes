ce6a6caa327043f238afc687febabaf40d30217f
AxelAlavedra <axel.alavedra@gmail.com>
2021-03-29 19:51:40 +0200

    Working on spin puzzle

--- Assets/SpinActivator.cs
+++ Assets/SpinActivator.cs
@@ -7,21 +7,26 @@ public class SpinActivator : MonoBehaviour
     public PointsPuzzle puzzle;
     public float spinForce = 5.0f;
 
+    GameObject grabber;
+
     public void Spin(float force)
     {
         float displacement = force * Time.deltaTime;
         if(puzzle.CanMove(displacement))
         {
-            transform.Rotate(transform.up, displacement * 20);
+            transform.Rotate(transform.up, -displacement * 20);
             puzzle.Move(displacement);
         }
     }
 
+    public void OnGrab(GameObject g)
+    {
+        grabber = g;
+        //disable control etc
+    }
     public void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
-            Spin(-spinForce);
-        else if(Input.GetKey(KeyCode.E))
-            Spin(spinForce);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Spin(horizontalInput * spinForce);
     }
 }
