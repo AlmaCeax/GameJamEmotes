8aba9eae6135cbc2346ef3b758af8b29c6b802d9
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2021-03-31 18:39:24 +0200

    Update CereMultiPlatformMove.cs

--- Assets/EmotesGame/Scripts/Puzzles/CereMultiPlatformMove.cs
+++ Assets/EmotesGame/Scripts/Puzzles/CereMultiPlatformMove.cs
@@ -26,36 +26,42 @@ public class CereMultiPlatformMove : MonoBehaviour
         {
             if(left && count > 0)
             {
-                transform.position = new Vector3(transform.position.x + (1 * Time.deltaTime), transform.position.y, transform.position.z);
-                if(player1)
-                    player1.Move(transform.right * Time.deltaTime);
-                if(player2)
-                    player2.Move(transform.right * Time.deltaTime);
-                count--;
-            }
-            else if(!left && count < length)
-            {
+                
                 transform.position = new Vector3(transform.position.x - (1 * Time.deltaTime), transform.position.y, transform.position.z);
                 if(player1)
                     player1.Move(-transform.right * Time.deltaTime);
                 if(player2)
                     player2.Move(-transform.right * Time.deltaTime);
+                count--;
+            }
+            else if(!left && count < length)
+            {
+                transform.position = new Vector3(transform.position.x + (1 * Time.deltaTime), transform.position.y, transform.position.z);
+                if(player1)
+                    player1.Move(transform.right * Time.deltaTime);
+                if(player2)
+                    player2.Move(transform.right * Time.deltaTime);
                 count++;
             }
-            if (count == length)
+            if (count == 0)
                 left = true;
-            else if (count == 0)
+            else if (count == length)
                 left = false;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        move = true;
         if (player1 == null)
             player1 = other.GetComponent<CharacterController>();
         else if(other.GetComponent<CharacterController>() != player1)
             player2 = other.GetComponent<CharacterController>();
+        if (player1 && player2)
+        {
+            move = true;
+            left = true;
+        }
+            
     }
 
     private void OnTriggerExit(Collider other)
@@ -65,8 +71,11 @@ public class CereMultiPlatformMove : MonoBehaviour
         if (other.GetComponent<CharacterController>() == player2)
             player2 = null;
         
-        if (!player1 && !player2)
-            move = false;
+        if (!player1 || !player2)
+        {
+            move = true;
+            left = false;
+        }
     }
 
 
