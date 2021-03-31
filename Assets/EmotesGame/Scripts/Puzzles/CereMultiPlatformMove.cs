33970ddae3b3355ac3c5a1b58dd8d37c8a98c799
AxelAlavedra <axel.alavedra@gmail.com>
2021-03-31 18:48:39 +0200

    Update CereMultiPlatformMove.cs

--- Assets/EmotesGame/Scripts/Puzzles/CereMultiPlatformMove.cs
+++ Assets/EmotesGame/Scripts/Puzzles/CereMultiPlatformMove.cs
@@ -52,18 +52,18 @@ public class CereMultiPlatformMove : MonoBehaviour
             {       
                 transform.position = new Vector3(transform.position.x - (1 * Time.deltaTime), transform.position.y, transform.position.z);
                 if(player1 && player1.pView.IsMine)
-                    player1.Move(-transform.right * Time.deltaTime);
+                    player1.movement.controller.Move(-transform.right * Time.deltaTime);
                 if(player2 && player1.pView.IsMine)
-                    player2.Move(-transform.right * Time.deltaTime);
+                    player2.movement.controller.Move(-transform.right * Time.deltaTime);
                 count--;
             }
             else if(!left && count < length)
             {
                 transform.position = new Vector3(transform.position.x + (1 * Time.deltaTime), transform.position.y, transform.position.z);
                 if(player1 && player1.pView.IsMine)
-                    player1.Move(transform.right * Time.deltaTime);
+                    player1.movement.controller.Move(transform.right * Time.deltaTime);
                 if(player2 && player2.pView.IsMine)
-                    player2.Move(transform.right * Time.deltaTime);
+                    player2.movement.controller.Move(transform.right * Time.deltaTime);
 
                 count++;
             }
